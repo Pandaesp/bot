202af3d9d1e0ec2e2281f34753461d914e9388a7
Mikhail Durbazhev <mdurbazhev@ozon.ru>
2023-01-31 16:46:04 +0400

    Move commands to separate package

--- cmd/bot/main.go
+++ cmd/bot/main.go
@@ -4,6 +4,7 @@ import (
 	"log"
 	"os"
 
+	"github.com/Pandaesp/bot/internal/app/commands"
 	"github.com/Pandaesp/bot/internal/service/product"
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 )
@@ -28,48 +29,20 @@ func main() {
 
 	productService := product.NewService()
 
+	commander := commands.NewCommander(bot, productService)
+
 	for update := range updates {
 		if update.Message != nil { // If we got a message
 			log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
 
 			switch update.Message.Command() {
 			case "help":
-				helpComand(bot, update.Message)
+				commander.Help(update.Message)
 			case "list":
-				listComand(bot, update.Message, productService)
+				commander.List(update.Message)
 			default:
-				defaultBehavior(bot, update.Message)
+				commander.Default(update.Message)
 			}
 		}
 	}
-}
-
-func helpComand(bot *tgbotapi.BotAPI, inputMessage *tgbotapi.Message) {
-	msg := tgbotapi.NewMessage(inputMessage.Chat.ID,
-		"/help - help"+"\n"+"/list - list products")
-
-	bot.Send(msg)
-}
-
-func listComand(bot *tgbotapi.BotAPI, inputMessage *tgbotapi.Message, productService *product.Service) {
-
-	outputMsgText := "Here all the products: \n\n"
-
-	products := productService.List()
-
-	for _, p := range products {
-		outputMsgText += p.Title
-		outputMsgText += "\n"
-	}
-
-	msg := tgbotapi.NewMessage(inputMessage.Chat.ID, outputMsgText)
-
-	bot.Send(msg)
-}
-
-func defaultBehavior(bot *tgbotapi.BotAPI, inputMessage *tgbotapi.Message) {
-	msg := tgbotapi.NewMessage(inputMessage.Chat.ID, "Unknown command")
-	msg.ReplyToMessageID = inputMessage.MessageID
-
-	bot.Send(msg)
 }
